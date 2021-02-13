b61a1045105a40c91bb7159e7f85e9ced2d47efe
Anisia <sc.anisia@gmail.com>
2021-02-13 11:15:52 +0100

    Add service call to get list of articles

--- app/src/main/java/com/example/nytimes/api/NYTimesService.kt
+++ app/src/main/java/com/example/nytimes/api/NYTimesService.kt
@@ -1,10 +1,14 @@
 package com.example.nytimes.api
 
+import com.example.nytimes.data.model.Article
+import com.example.nytimes.data.remote.ArticleResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface NYTimesService {
 
     @GET("home.json")
-    suspend fun getArticles(@Header("api-key") token : String)
+    suspend fun getArticles(@Query("api-key") token: String): Response<ArticleResponse<List<Article>>>
 }
